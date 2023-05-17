f7c98ea1ce2468092acca443b7d7bb545b37ff08
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-05-17 14:13:44 +0000

    init pipeline

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello word")
+import os
+from fastapi import FastAPI, HTTPException, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = FastAPI()
+
+access_token = os.environ.get("ACCESS_TOKEN")
+secret_channel = os.environ.get("SECRET_CHANNEL")
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+
+    # handle webhook body
+    try:
+        handler.handle(body.decode("utf-8"), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature. Please check your channel access token/channel secret.")
+        
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
