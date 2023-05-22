5d7eddb096c1dfe644e32b30a8ae5e43068c21f9
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-05-22 14:05:34 +0000

    feat: add azure custom vision on line bot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import requests
+
 from fastapi import FastAPI, HTTPException, Request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -37,16 +39,33 @@ def handle_message(event):
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     message_content = line_bot_api.get_message_content(event.message.id)
-    print(message_content)
 
-    # Use your function to classify the image
-    # food_name, carb_estimation = classify_image(message_content.content)
-    food_name = "food_name"
-    carb_estimation = "carb_estimation"
+    # Azure endpoint and headers
+    endpoint = os.environ.get("AZURE_PREDICT_URL")
+    headers = {
+        'Prediction-Key': os.environ.get("AZURE_PREDICT_KEY")',
+        'Content-Type': 'application/octet-stream'
+    }
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=f"The food is {food_name} with an estimated {carb_estimation}g of carbs"))
+    # Send image content to Azure Custom Vision
+    response = requests.post(endpoint, headers=headers, data=message_content.content)
+
+    # Process the response from Azure Custom Vision
+    if response.status_code == 200:
+        prediction_result = response.json()
+        # Extract information from the result
+        # Here I assume the prediction_result has a 'predictions' key with a list of predictions
+        # You may need to adjust this based on the actual structure of prediction_result
+        best_prediction = max(prediction_result['predictions'], key=lambda x: x['probability'])
+        food_name = best_prediction['tagName']
+        # carb_estimation = best_prediction['probability']  # Adjust this based on how you get the carb estimation
+        carb_estimation = '200'
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"The food is {food_name} with an estimated {carb_estimation}g of carbs"))
+    else:
+        print(f"Error making prediction: {response.status_code}, {response.text}")
 
 if __name__ == "__main__":
     import uvicorn
