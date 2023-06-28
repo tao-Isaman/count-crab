c87dcc2e586edf5c0576dd1af699e3fb052453f7
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-06-28 11:43:19 +0000

    update new api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from fastapi import FastAPI, HTTPException, Request, File
+from fastapi import FastAPI, HTTPException, Request, File, Form, UploadFile
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage
